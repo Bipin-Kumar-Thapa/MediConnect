984affcead7bfc4c1d6821bd0f847ea2e4f82dac
Bipin-Kumar-Thapa <bipinkumarthapa736@gmail.com>
2026-02-24 10:12:57 +0545

    Shifted to PostgreSQL

--- mediconnect/settings.py
+++ mediconnect/settings.py
@@ -101,11 +101,23 @@ SITE_ID = 1
 
 WSGI_APPLICATION = 'mediconnect.wsgi.application'
 
-# Database
+# # Database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('DB_NAME', BASE_DIR / 'db.sqlite3'),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', ''),
     }
 }
 
